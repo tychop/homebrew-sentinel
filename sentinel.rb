a32dafd6ecc7987ff6b101819aca1a2f6222a579
Tycho Pandelaar <tychop@me.com>
2026-04-25 13:51:08 +0200

    Update DMG URL to use release asset naming

--- sentinel.rb
+++ sentinel.rb
@@ -2,7 +2,7 @@ cask "sentinel" do
   version "4.0.0"
   sha256 "b784a225cbe27e202c76ac62308e8d9b101a679a3c1ccbd8c302a1dd464fadca"
 
-  url "https://github.com/tychop/Sentinel/releases/download/v#{version}/Sentinel-59.dmg"
+  url "https://github.com/tychop/Sentinel/releases/download/v#{version}/Sentinel.dmg"
   name "Sentinel"
   desc "GitHub repository, workflow, and PR monitor for macOS"
   homepage "https://github.com/tychop/Sentinel"
